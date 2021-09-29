56b1b8817335f34526dc69ed700c0797b74e99bf
wuyang <2414821184@qq.com>
2021-09-30 07:37:37 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/wymusic/' : '/',
     chainWebpack: config => {
         //产品发布阶段main入口
         config.when(process.env.NODE_ENV === 'production', config => {
